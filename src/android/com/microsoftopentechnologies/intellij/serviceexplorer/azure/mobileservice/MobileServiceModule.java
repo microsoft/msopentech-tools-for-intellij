c181d602ebfdc89d6e0a75ecf5d5fdcdb78b8c30
Leonardo M. Tilli <leotilli@gmail.com>
2015-03-31 17:35:40 -0300

    [Android] Fix certificate loop after cancel OpenSSL

--- src/android/com/microsoftopentechnologies/intellij/serviceexplorer/azure/mobileservice/MobileServiceModule.java
+++ src/android/com/microsoftopentechnologies/intellij/serviceexplorer/azure/mobileservice/MobileServiceModule.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2014 Microsoft Open Technologies Inc.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.microsoftopentechnologies.intellij.serviceexplorer.azure.mobileservice;
@@ -49,10 +49,11 @@ public class MobileServiceModule extends Node {
 
         // load all mobile services
         ArrayList<Subscription> subscriptionList = AzureRestAPIManagerImpl.getManager().getSubscriptionList();
-        if(subscriptionList != null) {
+
+        if (subscriptionList != null) {
             for (Subscription subscription : subscriptionList) {
                 List<MobileService> mobileServices = AzureRestAPIManagerImpl.getManager().getServiceList(subscription.getId());
-                for(MobileService mobileService : mobileServices) {
+                for (MobileService mobileService : mobileServices) {
                     addChildNode(new MobileServiceNode(this, mobileService));
                 }
             }
@@ -71,7 +72,7 @@ public class MobileServiceModule extends Node {
         public void actionPerformed(NodeActionEvent e) {
             // check if we have a valid subscription handy
             AzureRestAPIManager apiManager = AzureRestAPIManagerImpl.getManager();
-            if(apiManager.getAuthenticationMode() == AzureAuthenticationMode.Unknown) {
+            if (apiManager.getAuthenticationMode() == AzureAuthenticationMode.Unknown) {
                 UIHelper.showException("Please configure an Azure subscription by right-clicking on the \"Azure\" " +
                         "node and selecting \"Manage subscriptions\".", null, "No Azure subscription found");
                 return;
@@ -79,10 +80,10 @@ public class MobileServiceModule extends Node {
 
             try {
                 ArrayList<Subscription> subscriptions = apiManager.getSubscriptionList();
-                if(subscriptions == null || subscriptions.isEmpty()) {
+                if (subscriptions == null || subscriptions.isEmpty()) {
                     UIHelper.showException("No active Azure subscription was found. Please enable one more Azure " +
-                            "subscriptions by right-clicking on the \"Azure\" " +
-                            "node and selecting \"Manage subscriptions\".",
+                                    "subscriptions by right-clicking on the \"Azure\" " +
+                                    "node and selecting \"Manage subscriptions\".",
                             null, "No active Azure subscription found");
                     return;
                 }
