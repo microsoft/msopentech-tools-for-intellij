7e9aa26d92a06595d983dc88d967d4a10afeab3a
akrakovsky <arielk@lagash.com>
2015-04-06 17:30:29 -0300

    First version of Queue editor

---
+++ src/android/com/microsoftopentechnologies/intellij/helpers/storage/QueueFileEditor.java
@@ -0,0 +1,239 @@
+/**
+ * Copyright 2014 Microsoft Open Technologies Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.microsoftopentechnologies.intellij.helpers.storage;
+
+import com.intellij.codeHighlighting.BackgroundEditorHighlighter;
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorLocation;
+import com.intellij.openapi.fileEditor.FileEditorState;
+import com.intellij.openapi.fileEditor.FileEditorStateLevel;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import com.microsoftopentechnologies.intellij.model.storage.Queue;
+import com.microsoftopentechnologies.intellij.model.storage.QueueMessage;
+import com.microsoftopentechnologies.intellij.model.storage.StorageAccount;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeListener;
+
+public class QueueFileEditor implements FileEditor {
+    private Project project;
+    private StorageAccount storageAccount;
+    private Queue queue;
+    private JPanel mainPanel;
+    private JButton decueueMessageButton;
+    private JButton refreshButton;
+    private JButton addMessageButton;
+    private JButton clearQueueButton;
+    private JTable queueTable;
+
+    public void fillGrid() {
+        queueTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        DefaultTableModel model =  new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int i, int i1) {
+                return false;
+            }
+        };
+
+        model.addColumn("Id");
+        model.addColumn("Message Text Preview");
+        model.addColumn("Size");
+        model.addColumn("Insertion Time (UTC)");
+        model.addColumn("Expiration Time (UTC)");
+        model.addColumn("Dequeue count");
+
+        queueTable.setModel(model);
+        queueTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+        queueTable.getColumnModel().getColumn(1).setPreferredWidth(100);
+        queueTable.getColumnModel().getColumn(2).setPreferredWidth(10);
+        queueTable.getColumnModel().getColumn(3).setPreferredWidth(40);
+        queueTable.getColumnModel().getColumn(4).setPreferredWidth(40);
+        queueTable.getColumnModel().getColumn(5).setPreferredWidth(20);
+
+        JTableHeader tableHeader = queueTable.getTableHeader();
+        Dimension headerSize = tableHeader.getPreferredSize();
+        headerSize.setSize(headerSize.getWidth(), 18);
+        tableHeader.setPreferredSize(headerSize);
+        tableHeader.setReorderingAllowed(false);
+        tableHeader.setResizingAllowed(true);
+
+        queueTable.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                if (me.getComponent() instanceof JTable) {
+                    int r = queueTable.rowAtPoint(me.getPoint());
+                    if (r >= 0 && r < queueTable.getRowCount()) {
+                        queueTable.setRowSelectionInterval(r, r);
+                    } else {
+                        queueTable.clearSelection();
+                    }
+
+                    int rowIndex = queueTable.getSelectedRow();
+                    if (rowIndex < 0)
+                        return;
+
+                    if (me.getClickCount() == 2) {
+                        editMessageText();
+                    }
+
+                    if (me.getButton() == 3) {
+
+                        QueueMessage message = getSelectedQueueMessage();
+                        if (message != null) {
+                            JPopupMenu popup = createTablePopUp();
+                            popup.show(me.getComponent(), me.getX(), me.getY());
+                        }
+                    }
+                }
+            }
+        });
+
+        queueTable.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+                    editMessageText();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+            }
+        });
+
+    }
+
+    private JPopupMenu createTablePopUp() {
+        return null;
+    }
+
+    private QueueMessage getSelectedQueueMessage() {
+        return null;
+    }
+
+    private void editMessageText() {
+
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+
+    public void setStorageAccount(StorageAccount storageAccount) {
+        this.storageAccount = storageAccount;
+    }
+
+    public void setQueue(Queue queue) {
+        this.queue = queue;
+    }
+
+    @NotNull
+    @Override
+    public JComponent getComponent() {
+        return mainPanel;
+    }
+
+    @Nullable
+    @Override
+    public JComponent getPreferredFocusedComponent() {
+        return mainPanel;
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return queue.getName();
+    }
+
+    @NotNull
+    @Override
+    public FileEditorState getState(@NotNull FileEditorStateLevel fileEditorStateLevel) {
+        return FileEditorState.INSTANCE;
+    }
+
+    @Override
+    public void setState(@NotNull FileEditorState fileEditorState) {}
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public void selectNotify() {}
+
+    @Override
+    public void deselectNotify() {}
+
+    @Override
+    public void addPropertyChangeListener(@NotNull PropertyChangeListener propertyChangeListener) {}
+
+    @Override
+    public void removePropertyChangeListener(@NotNull PropertyChangeListener propertyChangeListener) {}
+
+    @Nullable
+    @Override
+    public BackgroundEditorHighlighter getBackgroundHighlighter() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public FileEditorLocation getCurrentLocation() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public StructureViewBuilder getStructureViewBuilder() {
+        return null;
+    }
+
+    @Override
+    public void dispose() {}
+
+    @Nullable
+    @Override
+    public <T> T getUserData(@NotNull Key<T> key) {
+        return null;
+    }
+
+    @Override
+    public <T> void putUserData(@NotNull Key<T> key, @Nullable T t) {}
+
+}
