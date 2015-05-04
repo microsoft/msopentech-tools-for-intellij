3ce07df1386b6ccd9930db85c90a07b72717a852
lenala <lena.lakhno@gmail.com>
2015-05-04 09:08:04 -0700

    TFS 3071: "Add" and "Edit" buttons are disabled in subscriptions tab in publish wizard

--- src/azure/com/microsoftopentechnologies/intellij/ui/SubscriptionsPanel.java
+++ src/azure/com/microsoftopentechnologies/intellij/ui/SubscriptionsPanel.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Microsoft Open Technologies Inc.
+ * Copyright 2015 Microsoft Open Technologies Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -42,7 +42,6 @@ public class SubscriptionsPanel implements AzureAbstractPanel {
     private JTable subscriptionsTable;
     private JButton importButton;
     private JButton addButton;
-    private JButton editButton;
     private JButton removeButton;
 
     private Project myProject;
